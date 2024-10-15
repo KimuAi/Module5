6e141865f00a96d42843a810b060b53a19451732
Kimberley Thill <ThillKimberleyPro@gmail.com>
2024-10-15 11:38:38 +0200

    E1

---
+++ E1.mjs
@@ -0,0 +1,11 @@
+import * as readline from 'node:readline/promises';
+import { stdin as input, stdout as output } from 'node:process';
+const userInput = readline.createInterface({ input, output });
+
+let naam = await userInput.question("Wat is jouw naam? ")
+
+begroeting(naam)
+
+function begroeting(naam){
+   console.log("Hello, " + naam)
+}
